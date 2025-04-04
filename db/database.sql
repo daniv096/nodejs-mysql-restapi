914883c8909d80ba43c1c06223a2818d6bb8c09e
Daniv096e <daniv096@gmail.com>
2025-04-04 01:56:08 -0400

    Primera pruebas

---
+++ db/database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS companydb;
+
+USE companydb;
+
+CREATE TABLE employee (
+   id INT(11) NOT NULL AUTO_INCREMENT,
+   name VARCHAR(45) DEFAULT NULL,
+   salary INT(5) DEFAULT NULL,
+   PRIMARY KEY(id)
+);
+
+DESCRIBE employee;
+
+INSERT INTO employee VALUES 
+(1, 'JOHN', 1000 ),
+(2, 'PETER', 2000),
+(3, 'MARY', 3000),
+(4, 'JONE', 4000);
+
+
