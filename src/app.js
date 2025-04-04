2cd90b51f1bc3d08981c7c2713b04b43b859e9b3
Daniv096e <daniv096@gmail.com>
2025-04-04 02:56:21 -0400

    modificaion de api modificar

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ import express from 'express'
 import employeesRoutes from './routes/employees-routes.js'
 import indexRoutes from './routes/index-routes.js'
 
-import {PORT} from './config.js'
-
 const app = express()
 
 app.use(express.json())
 
 app.use(indexRoutes)
-app.use('/api',employeesRoutes)
+app.use('/api', employeesRoutes)
 
 app.use((req, res, next) => {
     res.status(404).json({
@@ -18,4 +16,4 @@ app.use((req, res, next) => {
 })
 
 
-export default app;
+export default app
--- src/controllers/empleyees-controlles.js
+++ src/controllers/empleyees-controlles.js
@@ -57,11 +57,12 @@ export const deleteEmployees = async (req,res) => {
     }
 }
 
-export const updateEmployees = async ( req, res) => {
-    try {
+export const updateEmployees = async ( req, res) => {    
     const {id} = req.params
     const {name, salary} = req.body
-    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?,nama), salary = IFNULL(?, salary) WHERE id = ? ', [name, salary, id])
+    console.log(id,name,salary)
+    try {
+    const [result] = await pool.query('UPDATE employee SET name = IFNULL(?,name) , salary = IFNULL(?, salary) WHERE id = ? ', [name, salary, id])
     if (result.affectedRows === 0) return res.status(404).json({
         message: 'Employee not found'
     })
