cb590d564c826b07b85a08752dd1974c78c04055
Daniv096e <daniv096@gmail.com>
2025-04-08 22:00:06 -0400

    Anexo de login de usuarios 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app.js'
 import { PORT } from './config.js'
 
 
-const loginRoutes = require('./routes/login.routes');
+const loginRoutes = require('./routes/login-routes');
 app.use('/api', loginRoutes);
 
 app.listen(PORT)
